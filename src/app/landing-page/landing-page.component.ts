332d9da148c6d4184a617fa7440c4b1d72bcc17c
Manikandan Thiruvenkadaraj <manikandant@karyatech.com>
2020-05-07 10:05:37 +0530

    Default directives

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent {
 
-  test: boolean = true;
+  username: string = '<h1><b>Dinesh</b></h1>';
+  user = {
+    name: 'Raj',
+    age: 28
+  }
 
-  constructor() { }
+  employees = [
+    {
+      name: 'Raj',
+      age: 28
+    },
+    {
+      name: 'Dinesh',
+      age: 29
+    },
+    {
+      name: 'Vicky',
+      age: 30
+    }
+  ]
+
+  showDiv = true;
+  
+  constructor() { 
+    this.username = 'Aravind';
+  }
 
   ngOnInit(): void {
+    //Loading when the component initiate
+  }
+
+  ngAfterViewInit(){
+    //After loading the html content
   }
 
 }
